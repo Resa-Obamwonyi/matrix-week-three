e71dbf096fefe4b82511107cf1a15fe4f7cde8d3
adara-code <wakariuki.agnes@gmail.com>
2023-07-01 10:00:26 +0300

    added the logic in the EmailParser class

--- src/email_parser.py
+++ src/email_parser.py
@@ -1,15 +1,28 @@
 import re
 
-
 class EmailParser:
     # regex pattern that validate email
-    pattern = r''
+    pattern = r'([a-zA-Z0-9+/]+)(@{1})([a-zA-Z0-9]+\.{1}[c][o][m])'
     keys = ['username', 'domain']
 
     def parse(self, email):
         ''' parse email and return a dictionary of username and domain if valid and return None otherwise'''
         # Add implementation here ...
+        identifiedPattern = re.search(self.pattern,email)
+        if identifiedPattern:
+            new_list = list(identifiedPattern.groups())
+            new_list.remove('@')
 
-    def convert_to_dict(self):
+            self.convert_to_dict(self.keys, new_list)
+        else:
+            print("Nopity Nope") 
+
+
+    def convert_to_dict(self, keys, emailList):
         '''
           Takes in two lists as params and return them as a dictionary.'''
+        emailDictionary = zip(keys, emailList)
+        print(dict(emailDictionary))
+
+testEmail = EmailParser()
+testEmail.parse('jane+doe@yahoo.com')
--- tests/test_email_parser.py
+++ tests/test_email_parser.py
@@ -1,3 +1,15 @@
 # Import all required modules
-
 from unittest import TestCase
+from src.email_parser import EmailParser
+
+
+# sys.path.insert(0, '/Users/shee/Desktop/courses/we-tech-python tasks/matrix-week-three/src/email_parser.py')
+
+class Test(TestCase):
+    # testingParse = EmailParser()
+    def test_parse(self):
+        pass
+        # self.assertRegex(EmailParser.parse('jane+doe@yahoo.com'), '([a-zA-Z0-9+/]+)(@{1})([a-zA-Z0-9]+\.{1}[c][o][m])')
+        # self.assertRegex('johndoe@gmail.com', '([a-zA-Z0-9+/]+)(@{1})([a-zA-Z0-9]+\.{1}[c][o][m])')
+
+
